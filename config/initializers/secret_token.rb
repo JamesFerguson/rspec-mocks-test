5fa85d0af5dea3bce2d406f01250da02bb8058d9
JamesFerguson <james.ferguson7@gmail.com>
2014-04-11 15:49:31 +1000

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RspecMocksTest::Application.config.secret_key_base = '3d85335648e959c76a8b5a60a55423f29cd00c94373fdf8f7ce6542dfe6fd83a88e322a059d39518efd849c5a67caaa498ec6287eed05beec9e4c27329346118'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RspecMocksTest::Application.config.session_store :cookie_store, key: '_rspec-mocks-test_session'
